8fa3eb65fd633df80590f1406df26dd09d58983d
Sean Zheng <blackhorseya@gmail.com>
2021-09-02 18:21:25 +0800

    feat: impl at q2

---
+++ at/002.go
@@ -0,0 +1,29 @@
+package at
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+
+	return b
+}
+
+func q2(nums []int, target int) []int {
+	left, right, ret := 0, len(nums)-1, -1
+
+	for left < right {
+		sum := nums[left] + nums[right]
+		if sum < target {
+			ret = max(ret, sum)
+			left++
+		} else {
+			right--
+		}
+	}
+
+	if ret == -1 {
+		return nil
+	}
+
+	return []int{left, right + 1}
+}
---
+++ at/002_test.go
@@ -0,0 +1,36 @@
+package at
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_q2(t *testing.T) {
+	type args struct {
+		nums   []int
+		target int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "[1, 2, 3, 4] 4 then [1, 2]",
+			args: args{nums: []int{1, 2, 3, 4}, target: 4},
+			want: []int{1, 2},
+		},
+		{
+			name: "[1,2,3] 3 then []",
+			args: args{nums: []int{1, 2, 3}, target: 3},
+			want: nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := q2(tt.args.nums, tt.args.target); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("q2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
