350aa75821a535daf84ab1410b6ec94887d77716
Sean Zheng <blackhorseya@gmail.com>
2021-09-02 18:07:46 +0800

    feat: impl at q1

---
+++ at/001.go
@@ -0,0 +1,7 @@
+package at
+
+import "strconv"
+
+func q1(n int) string {
+	return strconv.FormatInt(int64(n), 7)
+}
---
+++ at/001_test.go
@@ -0,0 +1,32 @@
+package at
+
+import "testing"
+
+func Test_solution(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "20 then 26",
+			args: args{n: 20},
+			want: "26",
+		},
+		{
+			name: "-7 then -10",
+			args: args{n: -7},
+			want: "-10",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := q1(tt.args.n); got != tt.want {
+				t.Errorf("solution() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
